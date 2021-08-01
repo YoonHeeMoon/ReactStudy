e2f50a919d6dd7d6c251f5f1c60367731e5c6b71
YoonHeeMoon <yhm721@naver.com>
2021-08-01 20:10:10 +0900

    2021-08-01

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+
+    return(
+
+        <div className="Navbar">
+
+            <div className="navButton">계산기</div>
+            <div className="otherDiv navButton">달력</div>
+            <div className="otherDiv navButton">로그인</div>
+            <div className="otherDiv navButton">자기소개</div> 
+        </div>
+    );
+}
+export default Navbar;
