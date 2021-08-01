d23180a38c1cc14855e6ff4492a15e6a050242c7
YoonHeeMoon <yhm721@naver.com>
2021-08-01 21:53:25 +0900

    2021-08-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Table from "./Table";
+import Calendar from "./Calendar";
 import Navbar from "./Navbar";
 
 function App() {
   return (
       <div>
           <Navbar></Navbar>
+          
           <Table></Table>
+          <Calendar></Calendar>
       </div>
    );
 }
